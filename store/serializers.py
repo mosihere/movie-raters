571b17133f854c9b66fa889ea0751a40d6a774c1
Mosihere <Mostafakhoshhal@yahoo.com>
2024-04-07 18:39:30 +0330

    StarSerializer added and enhanced MovieSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,8 +1,18 @@
-from .models import Movie
+from .models import Movie, Star
 from rest_framework import serializers
 
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'imdb_rate', 'director']
+        fields = ['id', 'title', 'imdb_rate', 'director', 'stars']
+
+    stars = serializers.StringRelatedField(many=True)
+
+class StarSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Star
+        fields = ['id', 'first_name', 'last_name', 'movies']
+    
+    movies = serializers.StringRelatedField(many=True)
