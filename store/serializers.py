31792745960ea3a0e6bc51591697956cc1cd9683
Mosihere <Mostafakhoshhal@yahoo.com>
2024-04-07 12:06:17 +0330

    MovieSerializer added.

---
+++ store/serializers.py
@@ -0,0 +1,8 @@
+from .models import Movie
+from rest_framework import serializers
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'imdb_rate', 'director']
