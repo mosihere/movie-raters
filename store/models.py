08cd13028b2652b8c16725288bf77a02d5829fe4
Mosihere <Mostafakhoshhal@yahoo.com>
2024-04-07 12:06:06 +0330

    Movie models added

--- store/models.py
+++ store/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    imdb_rate = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    director = models.CharField(max_length=255, null=True, blank=True)
+    # actors = models.ForeignKey(Star, on_delete=models.PROTECT)
