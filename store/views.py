eb2f776b27ba6d095ad6bc4386f4b13ead415985
Mosihere <Mostafakhoshhal@yahoo.com>
2024-04-07 18:40:33 +0330

    StarViewSet added + enhanced queryset(Prefetch_related)

--- store/views.py
+++ store/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .serializers import MovieSerializer
-from .models import Movie
+from .serializers import MovieSerializer, StarSerializer
+from .models import Movie, Star
 
 
 class MovieViewSet(ModelViewSet):
     serializer_class = MovieSerializer
-    queryset = Movie.objects.all()
+    queryset = Movie.objects.prefetch_related('stars').all()
+
+
+class StarViewSet(ModelViewSet):
+    serializer_class = StarSerializer
+    queryset = Star.objects.prefetch_related('movies').all()
