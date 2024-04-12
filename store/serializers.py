e89ad1c4f73c787718a19361d7e40afb8f66387e
Mosihere <Mostafakhoshhal@yahoo.com>
2024-04-12 03:36:19 +0330

    ReviewSerializer added and small changes

--- store/serializers.py
+++ store/serializers.py
@@ -1,18 +1,25 @@
-from .models import Movie, Star
+from .models import Movie, Star, Review
 from rest_framework import serializers
-
+from . import views
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'imdb_rate', 'director', 'stars']
+        fields = ['id', 'title', 'imdb_rate', 'director', 'total_likes', 'stars', 'reviews']
 
-    stars = serializers.StringRelatedField(many=True)
+    stars = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    reviews = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
 class StarSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Star
         fields = ['id', 'first_name', 'last_name', 'movies']
     
     movies = serializers.StringRelatedField(many=True)
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Review
+        fields = ['id', 'title', 'description', 'owner', 'movie', 'total_likes']
