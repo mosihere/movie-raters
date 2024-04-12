1e3d4a255113b47bad075c6d25c9eeb473217b1e
Mosihere <Mostafakhoshhal@yahoo.com>
2024-04-12 03:37:46 +0330

    ReviewViewSet added and some query-optimization for previous views. eager_load needed data

--- store/views.py
+++ store/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .serializers import MovieSerializer, StarSerializer
-from .models import Movie, Star
+from .serializers import MovieSerializer, StarSerializer, ReviewSerializer
+from .models import Movie, Star, Review
 
 
 class MovieViewSet(ModelViewSet):
     serializer_class = MovieSerializer
-    queryset = Movie.objects.prefetch_related('stars').all()
+    queryset = Movie.objects.prefetch_related('stars').prefetch_related('likes').prefetch_related('reviews').all()
+
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 
 class StarViewSet(ModelViewSet):
     serializer_class = StarSerializer
     queryset = Star.objects.prefetch_related('movies').all()
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+    
+
+class ReviewViewSet(ModelViewSet):
+    serializer_class = ReviewSerializer
+    queryset = Review.objects.select_related('owner').select_related('movie').prefetch_related('likes').all()
