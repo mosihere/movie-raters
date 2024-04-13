fa7b326aecb86de91cc8b9f6b8e275d4c1d4edbf
Mosihere <Mostafakhoshhal@yahoo.com>
2024-04-14 01:48:30 +0330

    djoser urls and djoser_authentication urls routes registered

--- movie_raters/urls.py
+++ movie_raters/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
