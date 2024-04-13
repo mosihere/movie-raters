f7ecb449603607513b8a707b3c8663968d775ca5
Mosihere <Mostafakhoshhal@yahoo.com>
2024-04-14 01:46:41 +0330

    User model of core app registered

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "email"),
+            },
+        ),
+    )
+
+admin.site.register(User, UserAdmin)
