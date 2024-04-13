19b785b146506e6487cf72a317b283919742d871
Mosihere <Mostafakhoshhal@yahoo.com>
2024-04-13 18:01:44 +0330

    register Review for admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Movie, Star
+from .models import Movie, Star, Review
 
 
 admin.site.register(Movie)
 admin.site.register(Star)
+admin.site.register(Review)
