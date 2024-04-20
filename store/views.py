74e510066fcad0df542b967ace97bf94c5a9267d
Mosihere <Mostafakhoshhal@yahoo.com>
2024-04-21 02:05:02 +0330

    pagination added for records

--- store/views.py
+++ store/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import MovieSerializer, StarSerializer, ReviewSerializer
 from .models import Movie, Star, Review
+from .pagination import DefaultPagination
 
 
 class MovieViewSet(ModelViewSet):
     serializer_class = MovieSerializer
     queryset = Movie.objects.prefetch_related('stars').prefetch_related('likes').prefetch_related('reviews').all()
+    pagination_class = DefaultPagination
 
     def get_serializer_context(self):
         return {'request': self.request}
@@ -15,6 +17,7 @@ class MovieViewSet(ModelViewSet):
 class StarViewSet(ModelViewSet):
     serializer_class = StarSerializer
     queryset = Star.objects.prefetch_related('movies').all()
+    pagination_class = DefaultPagination
 
     def get_serializer_context(self):
         return {'request': self.request}
@@ -23,3 +26,4 @@ class StarViewSet(ModelViewSet):
 class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
     queryset = Review.objects.select_related('owner').select_related('movie').prefetch_related('likes').all()
+    pagination_class = DefaultPagination
