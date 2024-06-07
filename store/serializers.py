b15f126c5f6cc38678971e0857f3b9f725a5fd95
Mosihere <Mostafakhoshhal@yahoo.com>
2024-06-07 22:27:48 +0330

    Add CommentSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,10 +1,9 @@
-from .models import Movie, Star, Review
+from .models import Movie, Star, Review, Comment
+from core.models import User
 from rest_framework import serializers
 from . import views
 
 class MovieSerializer(serializers.ModelSerializer):
-
-
     class Meta:
         model = Movie
         fields = ['id', 'title', 'imdb_rate', 'director', 'total_likes', 'stars', 'reviews']
@@ -22,9 +21,16 @@ class StarSerializer(serializers.ModelSerializer):
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Review
         fields = ['id', 'description', 'owner', 'movie', 'total_likes']
 
     movie = serializers.HyperlinkedRelatedField(view_name='store:movie-detail', queryset=Movie.objects.all())
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'review', 'user', 'text', 'created_on']
+    
+    review = serializers.PrimaryKeyRelatedField(queryset=Review.objects.select_related('owner').all())
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
