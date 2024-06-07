3b63f633957bf1c947488830a9956666f0fab74f
Mosihere <Mostafakhoshhal@yahoo.com>
2024-06-07 22:28:43 +0330

    CommentViewSet added

--- store/views.py
+++ store/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .serializers import MovieSerializer, StarSerializer, ReviewSerializer
-from .models import Movie, Star, Review
+from .serializers import MovieSerializer, StarSerializer, ReviewSerializer, CommentSerializer
+from .models import Movie, Star, Review, Comment
 from .pagination import DefaultPagination
 
 
 class MovieViewSet(ModelViewSet):
     serializer_class = MovieSerializer
-    queryset = Movie.objects.prefetch_related('stars').prefetch_related('likes').prefetch_related('reviews').all()
+    queryset = Movie.objects.prefetch_related('stars', 'likes', 'reviews').all()
     pagination_class = DefaultPagination
 
     def get_serializer_context(self):
@@ -26,4 +26,11 @@ class StarViewSet(ModelViewSet):
 class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
     queryset = Review.objects.select_related('owner').select_related('movie').prefetch_related('likes').all()
+    pagination_class = DefaultPagination
+
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.select_related('review', 'user').all()
     pagination_class = DefaultPagination
