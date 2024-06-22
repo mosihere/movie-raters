fa549dd191af27cd8c85210f5541ff3981556198
Mosihere <Mostafakhoshhal@yahoo.com>
2024-06-22 19:58:22 +0330

    __str__ method added

--- store/models.py
+++ store/models.py
@@ -14,6 +14,9 @@ class Star(models.Model):
 class Genre(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
